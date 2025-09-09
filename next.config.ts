377bde27a5eaa46c36151e281ce8de885345435a
Refael Siagian <juarisiagian21106@gmail.com>
2025-09-09 12:12:06 +0700

    fix: ganti redirect home

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/', // Jika pengunjung mengakses path ini...
+        destination: '/home', // ...langsung alihkan ke path ini.
+        permanent: true, // Beri tahu browser & Google bahwa ini permanen (baik untuk SEO)
+      },
+    ]
+  },
 };
 
 export default nextConfig;
